e25503c4cde6b7eb2b3f53c45c4931a9141c8c13
DmytroPempus <145021219+DmytroPempus@users.noreply.github.com>
2023-10-27 22:42:15 +0300

    Add files via upload
    Додав мінімізацію зображень, завершив верхнє і ліве меню, зробив блок mentioned (з логотипом твітера )

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,65 @@
-var gulp = require ("gulp");
-//Створюємо тестовий таск
-gulp.task ('testTask', function () {
-    console.log ('This is a test task!');
-});
-//Запуск тасків за замовчуванням
-gulp.task ("default", ["testTask"]);zz
-
 const gulp = require('gulp');
+const rename = require('gulp-rename');
 const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+const browserSync = require('browser-sync').create();
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
 
-function buildStyles() {
-    return gulp.src('C:\\Users\\123\\Desktop\\lab_1_web(2)\\app\\sass')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('C:\\Users\\123\\Desktop\\lab_1_web(2)\\app\\css'));
-};
 
-exports.buildStyles = buildStyles;
-exports.watch = function () {
-    gulp.watch('./sass/**/*.scss', ['sass']);
-};
+function css_style() {
+    return gulp.src('./app/sass/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            errorLogToConsole: true,
+            outputStyle: 'compressed'
+        }).on('error', sass.logError))
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 2 versions']
+        }))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./app/css/'))
+        .pipe(browserSync.stream());
+}
+
+function jsTask() {
+    return gulp.src('src/js/*.js')
+        .pipe(concat('bundle.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'))
+        .pipe(browserSync.stream());
+}
+
+function compressImages() {
+    return gulp.src('./app/css/img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/img'));
+}
+
+
+function sync(done) {
+    browserSync.init({
+        server: {
+            baseDir: './app'
+        },
+        port: 3000
+    });
+    done();
+}
+
+function watchSass() {
+    gulp.watch('./app/sass/**/*.scss', css_style);
+    gulp.watch('./app/*.html').on('change', browserSync.reload);
+    gulp.watch('./app/js/*.js').on('change', browserSync.reload);
+}
+
+function defaultTask() {
+    gulp.parallel(css_style, sync, watchSass)();
+    gulp.watch('src/js/*.js', jsTask);
+    gulp.series(compressImages)
+}
+
+exports.default = defaultTask;
